1798d5dcd7b618ed2268b07ae601ccd84e713906
pietrochrist <pietrochrist@00db08a3-5b62-dd23-40b8-dcffe05755cd>
2012-06-21 17:57:04 +0000

    Desenvolvimento da AplControlarTurma

--- src/java/academico/controleinterno/cci/CtrlLetivo.java
+++ src/java/academico/controleinterno/cci/CtrlLetivo.java
@@ -5,7 +5,9 @@
 package academico.controleinterno.cci;
 
 import academico.controleinterno.cdp.Calendario;
+import academico.controleinterno.cdp.Turma;
 import academico.controleinterno.cgt.AplCadastrarCalendario;
+import academico.controleinterno.cgt.AplControlarTurma;
 import academico.util.Exceptions.AcademicoException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -24,7 +26,7 @@ public class CtrlLetivo {
     public static final int CONSULTAR = 2;
     private AplCadastrarCalendario apl = AplCadastrarCalendario.getInstance();
     private static CtrlLetivo instance = null;
-
+    private AplControlarTurma aplC = AplControlarTurma.getInstance();
     public static CtrlLetivo getInstance() {
         if (instance == null) {
             instance = new CtrlLetivo();
@@ -71,8 +73,50 @@ public class CtrlLetivo {
         map.put("obj", calendario);
         Executions.createComponents("/PagFormularioCalendario.zul", null, map);
     }
+     
+    
+    
+    public Turma incluirTurma(ArrayList<Object> args) throws AcademicoException {
+
+        return aplC.incluirTurma(args);
+    }
+
+    public Turma alterarTurma(Turma turma) throws Exception {
+        return aplC.alterarTurma(turma);
+    }
+
+    public void apagarTurma(Turma turma) throws Exception {
+        aplC.apagarTurma(turma);
+    }
+
+    public List<Turma> obterTurma() throws AcademicoException {
+        return aplC.obterTurmas();
+    }
+
+    public void abrirIncluirTurma() {
+        Map map = new HashMap();
+        map.put("tipo", CtrlLetivo.SALVAR);
+        Executions.createComponents("/PagFormularioTurma.zul", null, map);
+    }
+
+    public void abrirEditarTurma(Turma turma) {
+        Map map = new HashMap();
+        map.put("tipo", CtrlLetivo.EDITAR);
+        map.put("obj", turma);
+        Executions.createComponents("/PagFormularioTurma.zul", null, map);
+    }
+
+    public void abrirConsultarTurma(Turma turma) {
+        Map map = new HashMap();
+        Object put = map.put("tipo", CtrlLetivo.CONSULTAR);
+        map.put("obj", turma);
+        Executions.createComponents("/PagFormularioTurma.zul", null, map);
+    }
+    
     
     public void redirectPag(String url) {
         Executions.sendRedirect(url);
     }
+    
+    
 }
---
+++ src/java/academico/controleinterno/cgd/TurmaDAO.java
@@ -0,0 +1,7 @@
+package academico.controleinterno.cgd;
+
+import academico.controleinterno.cdp.Turma;
+import academico.util.persistencia.DAO;
+
+public interface TurmaDAO extends DAO<Turma> {
+}

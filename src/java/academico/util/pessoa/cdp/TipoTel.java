d531bf84f56106596c60fd628af94c924a22fd1d
rsoaresmaia <rsoaresmaia@00db08a3-5b62-dd23-40b8-dcffe05755cd>
2012-07-16 19:55:48 +0000

    Enum TipoTel.

---
+++ src/java/academico/util/pessoa/cdp/TipoTel.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package academico.util.pessoa.cdp;
+
+/**
+ *
+ * @author rodrigosmaia
+ */
+public enum TipoTel {
+    FIXO, CELULAR;
+}

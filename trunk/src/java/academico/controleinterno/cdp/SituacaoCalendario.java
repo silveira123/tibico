47c03781a652fdcc8ca92f7bf0b3b03440c29817
jpmirandamartins@gmail.com <jpmirandamartins@gmail.com@00db08a3-5b62-dd23-40b8-dcffe05755cd>
2012-07-18 19:01:39 +0000

    SituacaoCalendario

---
+++ trunk/src/java/academico/controleinterno/cdp/SituacaoCalendario.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package academico.controleinterno.cdp;
+
+/**
+ *
+ * @author jmiranda
+ */
+public enum SituacaoCalendario {
+    ABERTO, FECHADO
+}
